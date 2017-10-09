e3088813b9bb23a37f44ecf9bb30f2964091f578
Enrique Alvarez Pereira <>
2017-10-09 09:07:20 +0200

    Boletin 2 ejercicio 1 de otra forma

---
+++ Boletin2_1_1/src/boletin2_1/Boletin2_1.java
@@ -0,0 +1,16 @@
+
+package boletin2_1;
+
+
+public class Boletin2_1 {
+
+   
+    public static void main(String[] args) {
+      
+        float base = 4F;
+        float altura = 3F;
+        
+        System.out.println("Area= " + base*altura/2);
+    }
+    
+}
