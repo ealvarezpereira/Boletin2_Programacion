033257360f8ba3c22e3368d7148654a2f66a295b
Enrique Alvarez Pereira <>
2017-10-09 08:50:45 +0200

    Boletin 2 ejercicio 1

---
+++ Boletin2_1/src/boletin2_1/Boletin2_1.java
@@ -0,0 +1,18 @@
+
+package boletin2_1;
+
+
+public class Boletin2_1 {
+
+   
+    public static void main(String[] args) {
+      
+        float base = 4F;
+        float altura = 3F, area;
+        
+        area = base*altura/2;
+        
+        System.out.println("Area= " + area);
+    }
+    
+}
