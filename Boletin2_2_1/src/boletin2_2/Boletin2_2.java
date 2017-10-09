bd6bc4dd24c810be7203a823084680baae577a55
Enrique Alvarez Pereira <>
2017-10-09 09:11:28 +0200

    Boletin 2 ejercicio 2 de otra forma

---
+++ Boletin2_2_1/src/boletin2_2/Boletin2_2.java
@@ -0,0 +1,20 @@
+
+package boletin2_2;
+
+
+public class Boletin2_2 {
+
+  
+    public static void main(String[] args) {
+       
+        float lado = 3F, area;
+        
+        area = (float)Math.pow(lado, 2);
+        
+        System.out.println("El area es: " + area);
+        
+    }
+    
+}
+
+   
